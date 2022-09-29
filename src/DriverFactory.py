9a7523856a138b9c2d328a16b94b16d36873ce1f
markliuliu <markliu0000@gmail.com>
2022-09-30 00:02:29 +0800

    use 3.11.0 to avoid allow ip issue

--- src/DriverFactory.py
+++ src/DriverFactory.py
@@ -3,12 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from sys import platform
 
-
 def get_chrome_driver():
     if platform == "win32":
         options = Options()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_experimental_option("detach", True)
         return webdriver.Chrome(executable_path="../resource/chromedriver.exe", chrome_options=options)
     else:
-        return webdriver.Remote("http://selenium-standalone:4444/wd/hub", DesiredCapabilities.CHROME)
+        options = Options()
+        options.set_headless(True)
+        return webdriver.Remote("http://selenium-standalone:4444/wd/hub", DesiredCapabilities.CHROME, options= options)
